a4c3120d0aa218adbfd27241ba84bf519a328ee2
Tayyab Zaheer <tayyab.zaheer@eximap.com>
2015-07-22 17:56:56 +0300

    Enforcing the user authentication to the user's home page by using login_required decorator

--- boardgames/user/views.py
+++ boardgames/user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 # Create your views here.
 def home(request):
 	return render(request, "user/home.html")
